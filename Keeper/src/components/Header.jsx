d38fe00aee7832c882c54ad24efe549ae499f184
Cliff <cliff.bam92@gmail.com>
2020-06-20 10:21:40 -0600

    adding additional files

---
+++ Keeper/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StarsIcon from '@material-ui/icons/Stars';
+
+function Header() {
+  return (
+    <header>
+      <h1><StarsIcon />Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
